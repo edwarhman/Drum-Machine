07378c2c9ae2c49d76d7e1b8276016a4e99c5752
Emerson Warhman <edwarhman@gmail.com>
2022-08-09 23:48:17 -0400

    feat: style app

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,12 +2,28 @@ import React from 'react'
 // To verify props types
 import PropTypes from 'prop-types'
 
+const styles = {
+  height: '0',
+  paddingBottom: '100%',
+  position: 'relative',
+}
+
 function Button(props) {
   const { id, className, keyName } = props
 
   return (
-    <button id={id} className={className}>
-      {keyName}
+    <button id={id} style={styles} className={className}>
+      <div
+        style={{
+          position: 'absolute',
+          margin: 'auto 0',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        {keyName}
+      </div>
     </button>
   )
 }
