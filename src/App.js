c7811032f08a226f8f0535b7c237f150f4296ca0
Emerson Warhman <edwarhman@gmail.com>
2022-08-10 16:01:48 -0400

    feat: read audio tracks files and pass to buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import './App.css'
 import Button from './components/button'
+import { importAll } from './utils'
 
 const BUTTONS_KEYS = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
+const audioTracks = importAll(require.context('./audio', false, /\.(mp3)$/))
+const tracksNames = Object.keys(audioTracks)
+
+console.log(audioTracks)
+console.log(tracksNames)
 
 function App() {
+  const buttons = BUTTONS_KEYS.map((key, idx) => {
+    const trackName = tracksNames[idx]
+    const trackFile = audioTracks[trackName]
+
+    return (
+      <Button
+        trackFile={trackFile}
+        trackName={trackName}
+        key={key}
+        keyName={key}
+        className="drum-pad"
+        id={audioTracks[idx]}
+      ></Button>
+    )
+  })
+
   return (
     <div className="App" id="drum-machine">
       <div className="display-container">
         <h2>Display:</h2>
         <input readOnly id="display"></input>
       </div>
-      <div className="buttons-container">
-        {BUTTONS_KEYS.map((key) => (
-          <Button keyName={key} key={key}></Button>
-        ))}
-      </div>
+      <div className="buttons-container">{buttons}</div>
     </div>
   )
 }
