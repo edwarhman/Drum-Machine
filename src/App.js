f606b4a5f6b7cc7426ace237ba192474a9f48444
Emerson Warhman <edwarhman@gmail.com>
2022-08-09 23:46:39 -0400

    feat: add container for the display element

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ const BUTTONS_KEYS = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
 function App() {
   return (
     <div className="App" id="drum-machine">
-      <input readOnly id="display"></input>
+      <div className="display-container">
+        <h2>Display:</h2>
+        <input readOnly id="display"></input>
+      </div>
       <div className="buttons-container">
         {BUTTONS_KEYS.map((key) => (
           <Button keyName={key} key={key}></Button>
