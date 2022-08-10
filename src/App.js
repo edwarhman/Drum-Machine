bbac525d726da5a77eaffacd6ec7fcd572874250
Emerson Warhman <edwarhman@gmail.com>
2022-08-09 21:56:36 -0400

    feat: add drum machine basic elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello lint staged</p>
-      </header>
+    <div className="App" id="drum-machine">
+      <input readOnly id="display"></input>
+      <div className="buttons-container"></div>
     </div>
   )
 }
