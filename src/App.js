17b65bf099ca31e5ca8357a8db30acde16e613b7
Emerson Warhman <edwarhman@gmail.com>
2022-08-10 18:38:59 -0400

    feat: update display when lastAudio state changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react'
 import './App.css'
 import Button from './components/button'
 import { importAll } from './utils'
+import { useSelector } from 'react-redux'
 
 const BUTTONS_KEYS = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
 const audioTracks = importAll(require.context('./audio', false, /\.(mp3)$/))
 const tracksNames = Object.keys(audioTracks)
 
-console.log(audioTracks)
-console.log(tracksNames)
-
 function App() {
+  const lastAudio = useSelector((state) => state.drum.lastAudio)
   const buttons = BUTTONS_KEYS.map((key, idx) => {
     const trackName = tracksNames[idx]
     const trackFile = audioTracks[trackName]
@@ -31,7 +30,7 @@ function App() {
     <div className="App" id="drum-machine">
       <div className="display-container">
         <h2>Display:</h2>
-        <input readOnly id="display"></input>
+        <input readOnly id="display" value={lastAudio}></input>
       </div>
       <div className="buttons-container">{buttons}</div>
     </div>
